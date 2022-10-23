fd611739275ae9d4fe09033e20171e00a60081fb
Cpr4z <matvey.agarkov02@gmail.com>
2022-10-23 11:31:06 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './index.css';
 
-const chat = document.querySelector('.chat')
-const form = document.querySelector('.form')
-const input = document.querySelector('.from-input')
+const messages = document.querySelector('.messages');
+const form = document.querySelector('.form');   
+const back = document.querySelector('.back-button')
 
-form.addEventListener('submit', this.handleSubmit.bind(this))
-form.addEventListener('keypress', this.handleKeyPress.bind(this));
-from.addEventListener('DOMContentLoader', this.getMessagesFromLocalStorage.bind(this))
+form.addEventListener('submit', handleSubmit.bind(this));
+form.addEventListener('keypress', handleKeyPress.bind(this));
+window.addEventListener('DOMContentLoaded', getMessagesFromLocalStorage.bind(this));
+back-button.addEventListener('click',mainPage.bind(this));
 
 function handleKeyPress (event) {
     if (event.keyCode === 13) {
@@ -16,19 +17,25 @@ function handleKeyPress (event) {
 
 function handleSubmit(event){
     event.preventDefault()
+    const input = event.target.querySelector('.form-input');
     let time = new Date()
     let message = {
         'text':input.value,
         'date': `${time.getHours()}:${time.getMinutes()}`,
         'sender_name':'Agarkov Matvey'
+    };
+    if(input.value){
+    createMessageBlock(message);
+    input.value = '';
     }
-    createMessageBlock(message)
-    input.value = ''
 }
 
 function getMessagesFromLocalStorage(){
+    if(!checkLocalStorage){
+        return
+    }
     let messages = localStorage.getItem('messages')
-    if (messages == ''|| messages==null){
+    if (!messages){
         return
     }
     messages = JSON.parse(messages)
@@ -37,13 +44,31 @@ function getMessagesFromLocalStorage(){
     }
 }
 
-function saveMessageToLocalStorage(message){
-    let messages = localStorage.getItem('messages')
-    if (messages == '' || messages == null){
-        localStorage.setItem('messages', JSON.stringify({'all':[]}))
+function checkLocalStorage(){
+    try{
+        localStorage.setItem('Matvey', 'Agarkov')
+        localStorage.getItem('Matvey')
     }
-    messages = localStorage.getItem('messages')
-    messages = JSON.parse(messages)
+    catch(err){
+        return false
+    }
+    return true
+}
+
+function saveMessageToLocalStorage(message){
+    if(!checkLocalStorage){
+        return
+    }
+
+    let messages = localStorage.getItem('messages');
+    if (messages == '' || messages == null){
+        localStorage.setItem('messages', JSON.stringify({'all':[]}));
+    }
+    messages = localStorage.getItem('messages');
+    messages = JSON.parse(messages);
+    if(messages == undefined){
+        return
+    }
     messages.all.push(message)
     localStorage.setItem('messages', JSON.stringify(messages))
 }
@@ -55,7 +80,7 @@ function createMessageBlock(message, save=true){
 
     let message_text = document.createElement('div')
     message_text.className = 'message-text'
-    message_text.innerText = message.text
+    message_text.innerHTML = message.text
     message_block.append(message_text)
 
     let message_meta = document.createElement('div')
@@ -63,9 +88,9 @@ function createMessageBlock(message, save=true){
     let message_state = document.createElement('div')
     message_meta.className = 'message-meta'
     message_date.classname = 'message-date'
-    message_date.innerText = message.date
+    message_date.innerHTML= message.date
     message_state.className = 'message-state'
-    message_state.innerText = "<span class='material-icons'>done_all</span>"
+    message_state.innerHTML = "<span class='material-icons'>done_all</span>"
 
     message_meta.append(message_date)
     message_meta.append(message_state)
@@ -75,4 +100,8 @@ function createMessageBlock(message, save=true){
     if(save){
         saveMessageToLocalStorage(message)
     } 
+}
+
+function mainPage(event){
+    window.location.href = ''
 }
