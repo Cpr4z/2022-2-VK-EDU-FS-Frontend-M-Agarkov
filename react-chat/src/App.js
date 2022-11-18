fc52d6d2630be9a13b40b14dd38e08c46a6735cd
Матвей Агарков <matvey.agarkov02@gmail.com>
2022-11-19 00:30:38 +0300

    second commit

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { SidebarPage } from './pages/SidebarPage'
+import { ProfilePage } from './pages/ProfilePage'
+
+import { Layout } from './pages/Layout'
+
 
 function App() {
+
+  let [chatId, setChatId] = useState(0);
+
+  function openChat(id){
+    setChatId(id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Routes>
+          <Route path="/" element={<Layout chatId={chatId}/>}>
+            <Route index element={<SidebarPage openChat={openChat}/>} />
+            <Route path="profile" element={<ProfilePage />} />
+          </Route>
+        </Routes>
+      </>
   );
 }
+
 
 export default App;
