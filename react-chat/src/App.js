0dca7fa67f9412e67ebdc6299fcfa73f9442f7b3
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-11-19 15:24:30 +0300

    Update App.js

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,30 +1,30 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import styles from './App.module.css';
+import { useState } from 'react'
 
+import { ChatPage } from './Pages/ChatPage'
 import { SidebarPage } from './Pages/SidebarPage'
-import { ProfilePage } from './Pages/ProfilePage'
-
-import { Layout } from './Pages/Layout'
-
 
 function App() {
 
-  let [chatId, setChatId] = useState(0);
+    let [chatId, setChatId] = useState(0)
 
-  function openChat(id){
-    setChatId(id);
-  }
+    function openChat(id){
+        setChatId(id);
+    }
 
-  return (
-      <>
-        <Routes>
-          <Route path="/" element={<Layout chatId={chatId}/>}>
-            <Route index element={<SidebarPage openChat={openChat}/>} />
-            <Route path="profile" element={<ProfilePage />} />
-          </Route>
-        </Routes>
-      </>
-  );
+
+    return (
+        <>
+            <aside className={styles.allChats}>
+                <SidebarPage openChat={openChat}/>
+            </aside>
+            <section className={styles.chatPage}>
+                {chatId !== 0 &&
+                    <ChatPage chatId={chatId}/>
+                }
+            </section>
+        </>
+    )
 }
 
 
