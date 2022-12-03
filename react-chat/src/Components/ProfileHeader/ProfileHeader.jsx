cc0b6e761a2af39368c0742b06039df8f160b0c6
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-12-03 11:15:55 +0300

    Update ProfileHeader.jsx

--- react-chat/src/Components/ProfileHeader/ProfileHeader.jsx
+++ react-chat/src/Components/ProfileHeader/ProfileHeader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./ProfileHeader.module.scss";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 //const linkStyle = {
 //    alignSelf: "center",
