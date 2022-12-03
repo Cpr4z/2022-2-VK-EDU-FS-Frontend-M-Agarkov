5e84986938b12a28a78e1e44c195f4a3167efc46
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-12-03 10:50:49 +0300

    Update App.js

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,31 +1,82 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { SidebarPage } from './Pages/SidebarPage'
 import { ProfilePage } from './Pages/ProfilePage'
-import { ExtraButtons } from "./Pages/ExtraButtons";
+import { NavButtons } from "./Pages/NavButtons";
 
-import { Layout } from './Pages/Layout'
+import { ChatPage } from "./Pages/ChatPage";
 
+import {useDispatch, useSelector} from 'react-redux'
 
 function App() {
 
     const [chat, setChat] = useState({});
 
+    const dispatch = useDispatch()
+    const platform = useSelector(state => state)
+
+
     function openChat(chat){
-        console.log(chat);
+        console.log('открыт чат: (app.js)', chat);
         setChat(chat);
     }
 
+    const [screenSize, getDimension] = useState({
+        dynamicWidth: window.innerWidth,
+        dynamicHeight: window.innerHeight
+    });
+
+    const setDimension = () => {
+        getDimension({
+            dynamicWidth: window.innerWidth,
+            dynamicHeight: window.innerHeight
+        })
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', setDimension);
+        console.log(screenSize)
+
+        if (screenSize.dynamicWidth <= 1100) {
+            dispatch({type: 'isMobile', payload: 0});
+        }
+        else {
+            dispatch({type: 'isDesktop', payload: 0});
+        }
+
+        return(() => {
+            window.removeEventListener('resize', setDimension);
+        })
+    }, [screenSize])
+
     return (
         <>
-            <ExtraButtons />
-            <Routes>
-                <Route path="*" element={<Layout chat={chat}/>}>
-                    <Route index element={<SidebarPage openChat={openChat}/>} />
-                    <Route path="profile" element={<ProfilePage />} />
-                </Route>
-            </Routes>
+            <NavButtons />
+
+            {platform.isDesktop ?
+                <Routes>
+                    <Route exact path="/" element={<SidebarPage openChat={openChat}/>}>
+                        <Route path={`/chat/${chat.id}`} element={<ChatPage chat={chat} />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                        <Route
+                            path="*"
+                            element={<Navigate to="/" replace />}
+                        />
+                    </Route>
+                </Routes>
+                :
+                <Routes>
+                    <Route exact path="/" element={<SidebarPage openChat={openChat}/>} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                    <Route path={`/chat/${chat.id}`} element={<ChatPage chat={chat}/>} />
+                    <Route
+                        path="*"
+                        element={<Navigate to="/" replace />}
+                    />
+                </Routes>
+            }
         </>
     );
 }
