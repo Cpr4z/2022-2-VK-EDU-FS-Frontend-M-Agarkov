4321b27c03ca5a307004e0cc14f339c5c3900bb3
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-12-04 20:41:09 +0300

    Update SidebarPage.jsx

--- react-chat/src/Pages/SidebarPage/SidebarPage.jsx
+++ react-chat/src/Pages/SidebarPage/SidebarPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Outlet } from "react-router-dom";
 import { SidebarHeader } from "../../Components/SidebarHeader";
 import { SidebarBody } from "../../Components/SidebarBody";
 import { SidebarSearchField } from "../../Components/SidebarSearchField";
@@ -8,11 +8,15 @@ import styles from "./SidebarPage.module.scss";
 
 function SidebarPage({ openChat }) {
     return (
-        <aside className={styles.allChats}>
-            <SidebarHeader />
-            <SidebarSearchField />
-            <SidebarBody openChat={openChat} />
-        </aside>
+        <>
+            <aside className={styles.allChats}>
+                <SidebarHeader />
+                <SidebarSearchField />
+                <SidebarBody openChat={openChat} />
+            </aside>
+
+            <Outlet />
+        </>
     );
 }
 
