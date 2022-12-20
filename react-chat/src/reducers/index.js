9e84a2ed435fdc1dc36b8ca248346719d2341111
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-12-20 20:19:37 +0300

    Update index.js

--- react-chat/src/reducers/index.js
+++ react-chat/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import viewReduser from "./viewReduser";
 import messageReduser from "./messageReduser";
-import activeUserReducer from "./activeUserReducer";
+import activeUserReduser from "./activeUserReduser";
 
 export default combineReducers({
     viewReduser,
     messageReduser,
-    activeUserReducer,
+    activeUserReduser,
 });
