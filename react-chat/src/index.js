adbeadf23c609c2b43f6d0aa87896cc774cc42bf
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-12-20 19:27:35 +0300

    Update index.js

--- react-chat/src/index.js
+++ react-chat/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -13,7 +15,9 @@ root.render(
     // <React.StrictMode>
     <HashRouter>
         <Provider store={store}>
-            <App />
+            <GoogleOAuthProvider clientId="281550699425-ahuqjq9h6ic0utkmmdsofl7i8gdc894s.apps.googleusercontent.com">
+                <App />
+            </GoogleOAuthProvider>
         </Provider>
     </HashRouter>
     // </React.StrictMode>
