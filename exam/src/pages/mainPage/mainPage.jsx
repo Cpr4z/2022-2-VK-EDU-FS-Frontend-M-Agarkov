ad3de1006aeafb4da13c085adaed4278f552533f
m_agarkov <96927525+Cpr4z@users.noreply.github.com>
2022-12-30 15:05:25 +0300

    delete unused variables

--- exam/src/pages/mainPage/mainPage.jsx
+++ exam/src/pages/mainPage/mainPage.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import Header from '../../components/header/header'
 import {OpenHistoryButton} from "../../components/openHistoryButton/openHistoryButton";
 import {TranslateButton} from "../../components/TranslateButton/translateButton";
